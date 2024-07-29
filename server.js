8fa058b480194a32e79327fa9794e2bfdc5bcae5
himanshu280801 <139810233+himanshu280801@users.noreply.github.com>
2024-07-29 17:23:24 +0530

    TechMart-backend-api

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-import express from "express";
-import mongoose from "mongoose";
-import userRouter from "./Routes/user.js";
-import bodyParser from "express";
-import productRouter from './Routes/product.js';
-import cartRouter from './Routes/cart.js';
-import addressRouter from './Routes/address.js';
-import cors from "cors";
+import express from 'express'
+import mongoose from 'mongoose';
+import bodyParser from 'express'
+import userRouter from './Routes/user.js'
+import productRouter from './Routes/product.js'
+import cartRouter from './Routes/cart.js'
+import addressRouter from './Routes/address.js'
+import paymentRouter from './Routes/payment.js'
+import cors from 'cors';
 
 const app = express();
-app.use(bodyParser.json());
+
+app.use(bodyParser.json())
 
 app.use(cors({
   origin:true,
   methods:[ "GET","POST","PUT","DELETE"],
   credentials:true
 }))
-//Testing home route
-app.get("/", (req, res) => res.json({ message: "This is home route" }));
 
-//user Router
-app.use("/api/user", userRouter);
-//product Router
-app.use('/api/product',productRouter);
-//cart router
-app.use('/api/cart',cartRouter);
+// home testing route
+app.get('/',(req,res)=>res.json({messge:'This is home route'}))
+
+// user Router
+app.use('/api/user',userRouter)
+
+// product Router
+app.use('/api/product',productRouter)
+
+// cart Router
+app.use('/api/cart',cartRouter)
+
 // address Router
 app.use('/api/address',addressRouter)
 
-mongoose
-  .connect(
-    "mongodb+srv://himanshuug21ec:iA5BSPdNnHmEfqgE@cluster0.hbtxadg.mongodb.net/",
+// payment Router
+app.use('/api/payment',paymentRouter)
+
+mongoose.connect(
+ "mongodb+srv://himanshuug21ec:iA5BSPdNnHmEfqgE@cluster0.hbtxadg.mongodb.net/",
     {
       dbName: "MERN_E_COMMERCE_WEBSITE",
     }
-  )
-  .then(() => console.log("Mongodb connected successfully"));
+).then(()=>console.log("MongoDB Connected Succssfully...!")).catch((err)=>console.log(err));
+
 const port = 1000;
-app.listen(port, () => {
-  console.log(`server is running at port no ${port}`);
-});
+app.listen(port,()=>console.log(`Server is running on port ${port}`))
